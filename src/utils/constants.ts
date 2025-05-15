db830b0d4e8d1550c5844c8d5afe7fddd9e53cf0
allgjatap <allgjatapatricia@gmail.com>
2025-05-15 20:35:58 +0200

    client id

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
-export const GOOGLE_CLIENT_ID = import.meta.env.REACT_APP_GOOGLE_CLIENT_ID;
+// export const GOOGLE_CLIENT_ID = import.meta.env.REACT_APP_GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 
 export const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
